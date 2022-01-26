be32887e4ffe3d050f92f10c12fea82909009789
gary <gary.white@lodgit.com>
2022-01-26 13:01:07 +0100

    code working

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function testEven(n) {
     //Your awesome code here!
+    if (n % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 console.log(testEven(0)); // true "testEven for 0"
